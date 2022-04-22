6c58943c4301e3502f8fd1820db3ba1a2cf9bddc
Md.Merajul Islam Deep <merajulislamdeep@gmail.com>
2022-04-22 16:08:51 -0700

    js practice file init

---
+++ New.js
@@ -0,0 +1,127 @@
+// var friendsName = "Bangladesh";
+// console.log(friendsName)
+
+
+
+
+// var StrFridn= "Amadar name"
+// console.log(typeof StrFridn)
+
+
+
+// var numberType = 50;
+// console.log(typeof numberType)
+
+
+
+
+// var UpersCase = "bangladesh is the best site of the mether of frennser dot com in the world";
+// console.log(UpersCse.toUpperCase())
+
+
+// var LowerCase = "BANGLADESH IS THE BEST SITE OF THE METHER OF FRENNSER DOT COM IN THE WORLD";
+// console.log(LowerCase.toLowerCase())
+
+
+
+
+// var indexOf = "madarlandfor";
+// console.log(indexOf.indexOf("f"));
+
+
+// var banGla = "larachara haijalali";
+// console.log(banGla.split("haijalali"));
+
+// var mot1 = 150;
+
+// var mot2 = 150;
+
+// console.log(mot1 + mot2);
+
+
+
+// var num = "10.500556";
+// var numm= 10;
+
+// num = parseFloat(num);
+
+// var mot = num+numm;
+// console.log(mot);
+
+
+// var num = "10.500556";
+// var numm= 10;
+
+// num = parseInt(num);
+
+// var mot = num+numm;
+// console.log(mot);
+
+
+
+// var num1 = 50;
+//     var num2 = 60;
+//       num1 = "" + num1;
+//          console.log(typeof num1);
+
+
+// var dat = -50;
+
+// var sum = Math.abs(dat)
+// console.log(sum);
+
+
+// var round = 5.5;
+
+// var total = Math.round(round)
+// console.log(total);
+
+
+// var ceil = 0.00001;
+// var total = Math.ceil(ceil);
+// console.log(total);
+
+
+// var ceil = 0.999;
+// var total = Math.floor(ceil);
+// console.log(total);
+
+
+
+
+// var lotary = Math.random() * 100;
+// lotary = parseInt(lotary);
+// console.log(lotary);
+
+// var a = 10;
+// var b = 50;
+
+// if(a < b){
+//     console.log("You ar a Best Boy")
+// }
+
+
+
+// var a = 50;
+// var b = 20;
+
+// if(a < b){
+//     console.log("pinik Laga");
+// }
+// else{
+//     console.log("pinik Laga nh")
+// }
+
+
+
+var x = 5000;
+var y = 5000;
+
+if(x > y){
+    console.log("x boro");
+}
+else if(x < y){
+    console.log("y boro");
+}
+else{ console.log("thare both has Same")
+}
